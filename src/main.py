84b16b80995739a8fa817d84f77637b64c0db3e0
Kevin Xie <135569406+kevineix@users.noreply.github.com>
2025-07-11 14:51:05 +0200

    Update skeleton for RAG script

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 import os
-from helper.prompter import Prompter
+from helper.prompt import sys_prompt
+from helper.moderator import Moderator
 from helper.chats import Chat
 
 all_chats = Chat(messages_path='/Users/kevinxie/Desktop/MIT/multiparty-conversational-agent/New_IDS_Chat (Old studies)')
+moderator = Moderator(sys_prompt)
 
-group_1 = all_chats.get_group(1)
+def process_chat(chat: str):
+    """
+    Process a singular chat and inject whether intervention in needed
+    """
+
+    pass
+
+def main():
+    pass
